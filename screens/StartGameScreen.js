112c3b84685799d6934cd4696a7c459b8a849f76
mahaboob Basha Shaik <mahaboob.basha@dolcera.com>
2019-12-31 07:57:41 +0530

    * added basic input box form for guessing challenge

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,71 @@
+import React, { Component, useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+import Card from '../components/Card';
+import Colors from '../constants/colors';
+import Input from '../components/Input';
+
+const StartGameScreen = () => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+    const numberInuputHanlder = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    }
+
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a Number</Text>
+                    <Input style={styles.input} 
+                        blurOnSubmit 
+                        autoCapitalize='none' 
+                        autoCorrect={false} 
+                        keyboardType='number-pad' 
+                        maxLength={2}
+                        onChangeText={numberInuputHanlder}
+                        value={enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}><Button title="Reset" color={Colors.accent} onPress={ () => {}} /></View>
+                        <View style={styles.button}><Button title="Confirm" color={Colors.primary} onPress={ () => {}} /></View>
+                    </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,    
+        padding: 10,
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: "100%",
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    button: {
+        width: 100
+    },
+    input: {
+        width: 50,
+        textAlign: 'center'
+    }
+})
+
+export default StartGameScreen;
+
